9175c8c73b9cf9a609e0f2ad303b90d7447fbb33
desarrollo <santiagozapata0512@gmail.com>
2024-04-04 16:39:14 -0500

    cargo examen del 4 de abril porque no lo Forquie

---
+++ src/Home/Home.jsx
@@ -0,0 +1,70 @@
+import "./Home.css";
+
+import basedatos from "../utils/basedatos.json";
+
+export function Home() {
+  //logica para leer la base de datos
+  console.log();
+
+  return (
+    <>
+      <div className="banner text-white">
+        <h3>The barber's masters....</h3>
+        <h5>Since 2024</h5>
+      </div>
+
+      <section className="container my-5">
+        <div className="row">
+          <div className="col-12 text-center">
+            <img
+              src="../../src/assets/barbershop-logo.png"
+              alt="img"
+              className="w-50"
+            />
+            <hr />
+
+            <p>
+              Bienvenidos a The barber's masters, el lugar donde la tradición y
+              la innovación se encuentran para ofrecerte una experiencia de
+              corte y cuidado personal sin igual. En nuestro elegante espacio,
+              diseñado para el confort y la relajación de nuestros clientes, nos
+              dedicamos a revivir el arte del barbero con un toque
+              contemporáneo. Cada visita a nuestra barbería es más que un simple
+              corte de cabello; es una experiencia holística donde cada detalle
+              está pensado para tu bienestar. Desde el momento en que entras, te
+              sumergirás en un ambiente acogedor, acompañado de un servicio
+              personalizado que atiende tus necesidades específicas. Utilizamos
+              solo productos de la más alta calidad, seleccionados
+              meticulosamente para garantizar resultados excepcionales. Ya sea
+              que busques un corte clásico, un afeitado tradicional, o las
+              últimas tendencias en estilismo, nuestro equipo de profesionales
+              altamente calificados está aquí para superar tus expectativas. En
+              The barber's masters, no solo transformamos tu look, sino que te
+              ofrecemos un momento de pausa y placer en tu día a día. Visítanos
+              y descubre por qué somos más que una barbería; somos una
+              comunidad, un estilo de vida
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="container">
+        <section className="row row-cols-1 row-cols-md-5 g-3">
+          {basedatos.map(function (barbero) {
+            return (
+              <div className="col">
+                <div className="col card h-100 shadow px-2">
+                  <h3 className="text-center">{barbero.nombre}</h3>
+                  <p>Especialidad: {barbero.especialidad}</p>
+                  <p>Experiencia: {barbero.experiencia}</p>
+                  <p>Correo: {barbero.correo}</p>
+                  <p>Contacto: {barbero.contacto}</p>
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </section>
+    </>
+  );
+}
